cf0475e8e9fa03f64cd53791793a3621534b6141
Natselketi <natselketi@proton.me>
2026-05-13 18:05:08 -0300

    Set millennium to a specific commit

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nix-gaming.url = "github:fufexan/nix-gaming";
-    millennium.url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
+    millennium.url = "github:SteamClientHomebrew/Millennium?dir=packages/nix&ref=e2c66a276e579ee73c5151b01897bf63503aa12c";
     sls-steam = { url = "github:AceSLS/SLSsteam"; inputs.nixpkgs.follows = "nixpkgs"; };
     osu-lazer.url = "github:nixos/nixpkgs?ref=7413e8c550bd4b196fb4d4ba39243efd10ae3d5e";
     nur = { url = "github:nix-community/NUR"; inputs.nixpkgs.follows = "nixpkgs"; };
